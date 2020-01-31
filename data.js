bd2815274f82252d6571ff0665c1d8e80f33a4f9
Walter Hoerman <whoerman@gmail.com>
2020-01-31 13:14:51 -0500

    sarting file structure

---
+++ data.js
@@ -0,0 +1,185 @@
+let dataArray = [
+    [
+
+    ],
+
+    ["Why so JavaScript and Java have similar name?",
+        "JavaScript is a stripped-down version of Java",
+        "JavaScript's syntax is loosely based on Java's",
+        "They both originated on the island of Java",
+        "None of the above",
+        "JavaScript's syntax is loosely based on Java's"
+    ],
+
+    ["When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        "The user machine is running a Web browser",
+        "The Web server",
+        "A central machine deep within Netscape corporate offices",
+        "None of the above",
+        "The user machine is running a Web browser"
+    ],
+
+    ["How do you create a function in Javascritp?",
+        "create myFunction()",
+        "set myFunction()",
+        "function myFunction()",
+        "call myFunction()",
+        "function myFunction()"
+    ],
+
+    ["What are variables used for in JavaScript Programs?",
+        "Storing numbers, dates, or other values",
+        "Varying randomly",
+        "Causing high-school algebra flashbacks",
+        "None of the above",
+        "Storing numbers, dates, or other values"
+    ],
+
+    ["Which of the following can't be done with client-side JavaScript?",
+        "Validating a form",
+        "Sending a form's contents by email",
+        "Storing the form's contents to a database file on the server",
+        "None of the above",
+        "Storing the form's contents to a database file on the server"
+    ],
+
+
+    ["Which of the following are capabilities of functions in JavaScript?",
+        "Return a value",
+        "Accept parameters and Return a value",
+        "Accept parameters",
+        "None of the above",
+        "Accept parameters"
+    ],
+
+    ["10.  Which of the following is not a valid JavaScript variable name?",
+        "2names",
+        "_first_and_last_names",
+        "FirstAndLast",
+        "None of the above",
+        "2names"
+    ],
+
+    ["Why so JavaScript and Java have similar name?",
+        "JavaScript is a stripped-down version of Java",
+        "JavaScript's syntax is loosely based on Java's",
+        "They both originated on the island of Java",
+        "None of the above",
+        "JavaScript's syntax is loosely based on Java's"
+    ],
+
+    ["When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        "The user machine is running a Web browser",
+        "The Web server",
+        "A central machine deep within Netscape corporate offices",
+        "None of the above",
+        "The user machine is running a Web browser"
+    ],
+
+    ["How do you create a function in Javascritp?",
+        "create myFunction()",
+        "set myFunction()",
+        "function myFunction()",
+        "call myFunction()",
+        "function myFunction()"
+    ],
+
+    ["What are variables used for in JavaScript Programs?",
+        "Storing numbers, dates, or other values",
+        "Varying randomly",
+        "Causing high-school algebra flashbacks",
+        "None of the above",
+        "Storing numbers, dates, or other values"
+    ],
+
+    ["Which of the following can't be done with client-side JavaScript?",
+        "Validating a form",
+        "Sending a form's contents by email",
+        "Storing the form's contents to a database file on the server",
+        "None of the above",
+        "Storing the form's contents to a database file on the server"
+    ],
+
+
+    ["Which of the following are capabilities of functions in JavaScript?",
+        "Return a value",
+        "Accept parameters and Return a value",
+        "Accept parameters",
+        "None of the above",
+        "Accept parameters"
+    ],
+
+    ["10.  Which of the following is not a valid JavaScript variable name?",
+        "2names",
+        "_first_and_last_names",
+        "FirstAndLast",
+        "None of the above",
+        "2names"
+    ],
+    ["Why so JavaScript and Java have similar name?",
+        "JavaScript is a stripped-down version of Java",
+        "JavaScript's syntax is loosely based on Java's",
+        "They both originated on the island of Java",
+        "None of the above",
+        "JavaScript's syntax is loosely based on Java's"
+    ],
+
+    ["When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        "The user machine is running a Web browser",
+        "The Web server",
+        "A central machine deep within Netscape corporate offices",
+        "None of the above",
+        "The user machine is running a Web browser"
+    ],
+
+    ["How do you create a function in Javascritp?",
+        "create myFunction()",
+        "set myFunction()",
+        "function myFunction()",
+        "call myFunction()",
+        "function myFunction()"
+    ],
+
+    ["What are variables used for in JavaScript Programs?",
+        "Storing numbers, dates, or other values",
+        "Varying randomly",
+        "Causing high-school algebra flashbacks",
+        "None of the above",
+        "Storing numbers, dates, or other values"
+    ],
+
+    ["Which of the following can't be done with client-side JavaScript?",
+        "Validating a form",
+        "Sending a form's contents by email",
+        "Storing the form's contents to a database file on the server",
+        "None of the above",
+        "Storing the form's contents to a database file on the server"
+    ],
+
+
+    ["Which of the following are capabilities of functions in JavaScript?",
+        "Return a value",
+        "Accept parameters and Return a value",
+        "Accept parameters",
+        "None of the above",
+        "Accept parameters"
+    ],
+
+    ["10.  Which of the following is not a valid JavaScript variable name?",
+        "2names",
+        "_first_and_last_names",
+        "FirstAndLast",
+        "None of the above",
+        "2names"
+    ],
+
+    ["Why so JavaScript and Java have similar name?",
+        "JavaScript is a stripped-down version of Java",
+        "JavaScript's syntax is loosely based on Java's",
+        "They both originated on the island of Java",
+        "None of the above",
+        "JavaScript's syntax is loosely based on Java's"
+    ],
+
+    
+]
