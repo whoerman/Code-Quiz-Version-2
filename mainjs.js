160cd150dc0541ca8daac20189787c9863a45d7a
Walter Hoerman <whoerman@gmail.com>
2020-01-31 22:09:32 -0500

    putting in reset buttons

--- mainjs.js
+++ mainjs.js
@@ -5,15 +5,16 @@ var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimation
 var animationShake = "animated shake";
 var animationBounceOutLeft = "animated bounceOutLeft";
 var animationfadeInDown = "animated fadeInDown";
+var animationrubberBand = "animated rubberBand";
 
 //setting up the top buttons
 $("#wrong").hide();
 $('#startText').empty().text("Start the Quiz!");
 let countdown = 60;
-$('#timerText').empty().text(`Timer: ${countdown} seconds!`);
+$('#timerText').empty().text(` Timer: ${countdown} seconds!`);
 $("#score").hide();
 let scoreNum = 0;
-$('#scoreText').empty().text(`Your score: ${scoreNum} points`);
+$('#scoreText').empty().text(` Your score: ${scoreNum} points`);
 
 //start animation
 function startAnimation() {
@@ -24,18 +25,46 @@ function startAnimation() {
 };
 
 //score animation
-function scoreAnimation() {
+function scoreAnimation() {    
     $('#score').show("slow");
 };
 
 
 
-
+//buttons for starting, questions, and clear scores
 $(document).ready(function () {
     $('#start').on("click", function () {
         startAnimation();
         scoreAnimation();
         startTimer();
+    });
+    $("#optionA").on("click", function () {
+        if (dataArray[x][1] === dataArray[x][5]) {
+            URCorrect();
+            return;
+        };
+        URWrong();
+    });
+    $("#optionB").on("click", function () {
+        if (dataArray[x][2] === dataArray[x][5]) {
+            URCorrect();
+            return;
+        };
+        URWrong();
+    });
+    $("#optionC").on("click", function () {
+        if (dataArray[x][3] === dataArray[x][5]) {
+            URCorrect();
+            return;
+        };
+        URWrong();
+    });
+    $("#optionD").on("click", function () {
+        if (dataArray[x][4] === dataArray[x][5]) {
+            URCorrect();
+            return;
+        };
+        URWrong();
     });
 });
 
@@ -45,15 +74,38 @@ function startTimer() {
     $('#timer').addClass(animationShake);
     countdownID = setInterval(function () {
         countdown--;
-        $('#timerText').empty().text(`Timer: ${countdown} seconds!`);
+        $('#timerText').empty().text(` Timer: ${countdown} seconds!`);
         if (countdown < 10) {
             $('#timer').addClass(animationShake);
             $('#timer').css("background-color", "#ff2626");
         };
         if (countdown < 1) {
             clearInterval(countdownID);
-            $('#timerText').empty().text(`Game Over!`);
+            $('#timerText').empty().text(` Game Over!`);
             endGame();
         };
     }, 1000);
+};
+
+//If the question is answered correctly
+function URCorrect() {
+    scoreNum = scoreNum + 5;
+    countdown = countdown +2;
+    checkStatus();
+};
+
+//if the question is answered incorretly
+function URWrong() {
+    $("#wrong").show().hide("slow");
+    scoreNum = scoreNum -2;
+    countdown = countdown-5;
+    checkStatus();
+};
+
+function endGame () {
+
+};
+
+function checkStatus () {
+
 };
